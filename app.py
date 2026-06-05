19ea4a6551194169e14ac6b4f8ce28771fc63c87
vvikace <bezsensownekonto04@gmail.com>
2026-06-06 00:52:43 +0200

    Fix indentation in exception handling block

--- app.py
+++ app.py
@@ -295,5 +295,5 @@ Wygenerowano automatycznie przez Viral Detector by Wiktoria Cedro
         except Exception as e:
             st.error(f"Błąd: {e}. Upewnij się, że profil jest publiczny i wpisano poprawną nazwę.")
 
-            else: 
-                st.info("Wpisz nazwę publicznego profilu w panelu bocznym i kliknij przycisk, aby rozpocząć automatyczną analizę.")
+                 else: 
+                     st.info("Wpisz nazwę publicznego profilu w panelu bocznym i kliknij przycisk, aby rozpocząć automatyczną analizę.")
