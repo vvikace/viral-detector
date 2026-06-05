0c23fa6ab3d7dfc2e2347c9121d1014e94f3bbd0
vvikace <bezsensownekonto04@gmail.com>
2026-06-05 23:33:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -148,13 +148,19 @@ if analyze_button and target_profile:
                     key = st.secrets["SUPABASE_KEY"]
                     supabase: Client = create_client(url, key)
                     
-                    supabase.table("historia_analiz").insert({
-                        "profil": target_profile,
-                        "platforma": platform,
-                        "srednia": int(avg_engagement),
-                        "ostatni_post": int(latest_post["engagement"]),
-                        "v_score": float(v_score)
-                        }).execute()
+                    data_to_save = {
+                    "profil": target_profile,
+                    "platforma": platform,
+                    "srednia": int(avg_engagement),
+                    "ostatni_post": int(latest_post["engagement"]),
+                    "v_score": float(v_score)
+                }
+                
+                try:
+                    supabase.table("historia_analiz").upsert(data_to_save).execute()
+                    st.success("Dane zapisane w bazie!")
+                except Exception as e:
+                    st.error(f"Błąd zapisu: {e}")
     
                 except Exception as db_e:
                     st.warning(f"Nie udało się zapisać do bazy (sprawdź klucze API): {db_e}")
